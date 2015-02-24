40d08f2e7a2d4f33d114f142fe571fe0882aa0f1
Jordan <jordan.rosemond@gmail.com>
2015-02-24 15:25:33 +0100

    Creation fenetre Accueil & Facture

---
+++ Application/M2LStock/M2LStock/Accueil.xaml.vb
@@ -0,0 +1,14 @@
+﻿Public Class Accueil
+
+  Private Sub W_Accueil_Loaded(sender As Object, e As RoutedEventArgs) Handles W_Accueil.Loaded
+
+  End Sub
+
+  Private Sub Bt_Facture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_Facture.Click
+    Dim facture As New Facture
+    facture.Show()
+    Me.Hide()
+  End Sub
+
+ 
+End Class
--- Application/M2LStock/M2LStock/Facture.xaml.vb
+++ Application/M2LStock/M2LStock/Facture.xaml.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Facture
 
+  Private Sub W_Facture_Loaded(sender As Object, e As RoutedEventArgs) Handles W_Facture.Loaded
+    Me.WindowState = Windows.WindowState.Maximized
+
+  End Sub
+
+  Private Sub Bt_AjouterFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_AjouterFacture.Click
+
+  End Sub
+
+  Private Sub Bt_ModifierFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_ModifierFacture.Click
+
+  End Sub
+
+  Private Sub Bt_SupprimerFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_SupprimerFacture.Click
+
+  End Sub
 End Class
