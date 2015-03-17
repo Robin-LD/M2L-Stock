c5a7c0448b9872a8db67172cbd1513ca03e28435
Saucisson937 <jordan.rosemond@gmail.com>
2015-03-17 12:32:44 +0100

    Ajout d'un system de connexion à la BDD

--- Application/M2LStock/M2LStock/Facture.xaml.vb
+++ Application/M2LStock/M2LStock/Facture.xaml.vb
@@ -1,31 +1,41 @@
-﻿Public Class Facture
+﻿Imports MySql.Data.Entity
+Imports MySql.Data.MySqlClient
+Imports MySql.Data.Types
+
+
+Public Class Facture
 
 #Region "Evenement Click & Load "
 
-  Private Sub W_Facture_Loaded(sender As Object, e As RoutedEventArgs) Handles W_Facture.Loaded
-    Me.WindowState = Windows.WindowState.Maximized
+    Private Sub W_Facture_Loaded(sender As Object, e As RoutedEventArgs) Handles W_Facture.Loaded
+        Me.WindowState = Windows.WindowState.Maximized
+        Dim connection As String = "Server=localhost;Database=m2l-interne;Uid=root;Pwd="
+        Dim conn As New MySqlConnection
 
-  End Sub
+        conn.ConnectionString = connection
+        conn.Open()
 
-  Private Sub Bt_AjouterFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_AjouterFacture.Click
+    End Sub
 
-  End Sub
+    Private Sub Bt_AjouterFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_AjouterFacture.Click
 
-  Private Sub Bt_ModifierFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_ModifierFacture.Click
+    End Sub
 
-  End Sub
+    Private Sub Bt_ModifierFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_ModifierFacture.Click
 
-  Private Sub Bt_SupprimerFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_SupprimerFacture.Click
-    'commantaire
-    'Un autre commentaire
+    End Sub
 
-  End Sub
+    Private Sub Bt_SupprimerFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_SupprimerFacture.Click
+        'commantaire
+        'Un autre commentaire
 
-  Private Sub Bt_FactureRetour_Click(sender As Object, e As RoutedEventArgs) Handles Bt_FactureRetour.Click
-    Dim accueil As New Accueil
-    accueil.Show()
-    Me.Close()
-  End Sub
+    End Sub
+
+    Private Sub Bt_FactureRetour_Click(sender As Object, e As RoutedEventArgs) Handles Bt_FactureRetour.Click
+        Dim accueil As New Accueil
+        accueil.Show()
+        Me.Close()
+    End Sub
 
 #End Region
 
