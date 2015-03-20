d2dc8dec8d9a9031e0274aba2f7b102d229f9433
Saucisson937 <jordan.rosemond@gmail.com>
2015-03-20 07:50:18 +0100

    Mise a jour

--- Application/M2LStock/M2LStock/Facture.xaml.vb
+++ Application/M2LStock/M2LStock/Facture.xaml.vb
@@ -1,6 +1,8 @@
 ﻿Imports MySql.Data.Entity
 Imports MySql.Data.MySqlClient
 Imports MySql.Data.Types
+Imports System.Data.DataColumn
+Imports System.Media
 
 
 Public Class Facture
@@ -10,14 +12,43 @@ Public Class Facture
     Private Sub W_Facture_Loaded(sender As Object, e As RoutedEventArgs) Handles W_Facture.Loaded
         Me.WindowState = Windows.WindowState.Maximized
         Dim connection As String = "Server=localhost;Database=m2l-interne;Uid=root;Pwd="
-        Dim conn As New MySqlConnection
+        ' Dim conn As New MySqlConnection
 
-        conn.ConnectionString = connection
-        conn.Open()
+        'Try
+        '    conn.ConnectionString = connection
+        '    conn.Open()
+        'Catch ex As Exception
+        '    MsgBox("Impossible de se connecter à la base", vbOKOnly)
+        'End Try
+        
+
+        'Ajoute 5 colonnes
+
+        For i = 0 To 4
+            Dim column As New DataGridTextColumn
+            column.Header = "Numéro produit " + i.ToString
+            DataG_Facture.Columns.Add(column)
+            column.Width = DataG_Facture.ActualWidth / 5
+
+        Next i
+
 
     End Sub
 
     Private Sub Bt_AjouterFacture_Click(sender As Object, e As RoutedEventArgs) Handles Bt_AjouterFacture.Click
+        Dim mastring As String = "azerty"
+
+        'Ajoute 6 lignes
+        'For i = 0 To 5
+        Dim row As New RowDefinition
+        DataG_Facture.Items.Add(row)
+        ' Next i
+
+
+        'DataG_Facture.RowBackground = ColorConver(Colors.Blue)
+
+
+
 
     End Sub
 
